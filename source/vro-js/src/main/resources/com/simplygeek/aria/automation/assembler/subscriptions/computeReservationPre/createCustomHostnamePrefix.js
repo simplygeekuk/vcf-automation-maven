5b2669d7c50d2e1901b295b2ffa96a2a6e67e83c
Gavin Stephens <gavin.stephens@freelancercolo.local>
2024-05-14 11:34:55 +0100

    Added subscription for creating custom hostnames

---
+++ source/vro-js/src/main/resources/com/simplygeek/aria/automation/assembler/subscriptions/computeReservationPre/createCustomHostnamePrefix.js
@@ -0,0 +1,76 @@
+/**
+ * Write a brief description of the purpose of the action.
+ * @param {Properties} inputProperties - describe each parameter as in JSDoc format.
+ *
+ * @returns {void} - describe the return type as well
+ */
+(function (inputProperties) {
+    var log = new (System.getModule("com.simplygeek.log").Logger())(
+        "Action",
+        "createCustomHostnamePrefix"
+    );
+
+    // Get values from Input Properties
+    var customProps = inputProperties.get("customProperties");
+    var hostnamePrefix = customProps.get("hostnamePrefix");
+    log.debug("hostnamePrefix: " + hostnamePrefix);
+
+    // Get Provisioning Configuration
+    var configService = new (System.getModule("com.simplygeek.aria.orchestrator.configurations").ConfigElementService());
+    var provisioningConfigPath = "Simplygeek/Aria/Automation/Provisioning";
+    var restHostConfigPath = "Simplygeek/Aria/Orchestrator/Resthosts";
+    var provisioningDefaultsConfigElement = configService.getConfigElement("Defaults", provisioningConfigPath);
+
+    // Get Custom Naming
+    var customNamingProfile = configService.getConfigElementAttribute(provisioningDefaultsConfigElement, "customNamingProfile").value;
+
+    // Get Aria Automation Resthost
+    var ariaAutomationRestHostName = configService.getConfigElementAttribute(provisioningDefaultsConfigElement, "ariaAutomationRestHostName").value;
+    var ariaAutomationRestHost = System.getModule("com.simplygeek.rest").getRestHost(ariaAutomationRestHostName);
+    var ariaAutomationRestHostConfigElement = configService.getConfigElement(ariaAutomationRestHostName, restHostConfigPath);
+    var ariaAutomationApiToken = configService.getConfigElementAttribute(ariaAutomationRestHostConfigElement, "refreshToken").value;
+
+    var ariaIaasService = new (
+        System.getModule(
+            "com.simplygeek.aria.automation.assembler.iaas"
+        ).AriaAutomationAssemblerIaaSApiService())(ariaAutomationRestHost);
+
+    var locking = new (
+        System.getModule(
+            "com.simplygeek.aria.orchestrator.locking"
+        ).LockingService());
+    
+    ariaIaasService.createSession(ariaAutomationApiToken);
+    var customNamingProfile = ariaIaasService.getCustomNamingByName(customNamingProfile);
+    var customNamingProfileId = customNamingProfile.id;
+    var customNamingStartCounter = 0;
+    var customNamingIncrementStep = 1;
+
+    log.log(
+        "Adding hostname prefix '" + hostnamePrefix +
+        " to custom naming profile '" + customNamingProfile.name + "'"
+    );
+    if (ariaIaasService.checkCustomNamingPrefixExists(
+        customNamingProfileId,
+        hostnamePrefix)
+    ) {
+        this.log.log("The prefix '" + prefix + "' already exists");
+    } else {
+        locking.createLock("customNaming", customNamingProfileId);
+        try {
+            var updatedCustomNamingObject = ariaIaasService.addCustomNamingPrefixToTemplate(
+                customNamingProfileId,
+                hostnamePrefix + "-",
+                "COMPUTE",
+                customNamingStartCounter,
+                customNamingIncrementStep
+            );
+            log.log("Hostname prefix successfully added");
+
+        } catch (e) {
+            throw new Error("Unexpected error creating custom naming prefix: " + e);
+        } finally {
+            locking.removeLock("customNaming", customNamingProfileId);
+        }
+    }
+});
