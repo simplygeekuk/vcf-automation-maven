e22c97722b289bc544235cd7e04c9be41ccd791e
Gavin Stephens <gavin.stephens@freelancercolo.local>
2024-12-10 23:16:19 +0000

    Added Globals list

--- eslint.config.js
+++ eslint.config.js
@@ -26,7 +26,8 @@ export default [
                 RESTHostManager: "readonly",
                 RESTAuthenticationManager: "readonly",
                 VcVirtualDisk: "readonly",
-                AD_HostManager: "readonly"
+                AD_HostManager: "readonly",
+                ActiveDirectory: "readonly"
             }
         },
         rules: {
