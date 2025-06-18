3bc17e5e0b880480558346771da0f4138ec3b482
Gavin Stephens <gavin.stephens@freelancercolo.local>
2025-06-18 17:31:36 +0100

    Added parser script for test results

---
+++ scripts/parse-summary.js
@@ -0,0 +1,17 @@
+import { readFileSync } from "fs";
+import { argv } from "process";
+
+const log = readFileSync(argv[2], "utf8");
+const match = log.match(/(\d+)\s+specs,\s+(\d+)\s+failures/);
+
+if (match) {
+    const result = {
+        specs: parseInt(match[1], 10),
+        failures: parseInt(match[2], 10),
+        success: parseInt(match[2], 10) === 0
+    };
+    console.log(JSON.stringify(result, null, 2));
+} else {
+    console.error("Summary not found in log.");
+    process.exit(1);
+}
